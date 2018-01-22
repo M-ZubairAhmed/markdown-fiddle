3982e2ca0ef2094ff97ba4530a808f6460a16b51
z <ahmedzubair216@gmail.com>
2018-01-22 17:50:08 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export default class App extends React.Component {
+  state = {
+    inputText: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/Editor/Index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class Editor extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>s</div>
+      </div>
+    );
+  }
+}
